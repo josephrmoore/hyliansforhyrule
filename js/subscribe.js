2796f2cf6957a4516d8bc36ac7a0bc4af83aec5a
josephrmoore <josephrmoore@gmail.com>
2014-08-14 02:28:53 -0300

    new ideas

--- js/subscribe.js
+++ js/subscribe.js
@@ -8,42 +8,37 @@ jQuery(document).ready(function($){
 		site = "termina";
 	}
 	
-	$('#subscribe_to_newsletter').click(function(){
-		$('#modal').removeClass('display_none');
-		return false;
-	});
-	
-	$('#modal').click(function(){
-		$('#modal').addClass('display_none');
-		return false;
-	});
-	
-	$('#modal_content').click(function(e){
-		e.stopPropagation();
-		return false;
-	});
-	
-	$('#sign_up_form').submit(function(){
-		var email = $('#newsletter_email').val();
-		$.post( 
-             "mailinglist.php",
-             { "email": email, "site": site, "narrative": "first"},
-             function(data) {
-                $('#modal_content').html(data);
-             }
-
-        );
-        return false;
-	});
-	
-	function successMessage(){
+	if(localStorage.getItem('newsletter')=='hyrule' || localStorage.getItem('newsletter')=='termina'){
+		$('.newsletter').remove();
+		$('#modal').remove();
+	} else {
+		$('#subscribe_to_newsletter').click(function(){
+			$('#modal').removeClass('display_none');
+			return false;
+		});
 		
-	}
-	
-	function failureMessage(){
+		$('#modal').click(function(){
+			$('#modal').addClass('display_none');
+			return false;
+		});
 		
+		$('#modal_content').click(function(e){
+			e.stopPropagation();
+			return false;
+		});
+		
+		$('#sign_up_form').submit(function(){
+			var email = $('#newsletter_email').val();
+			$.post( 
+	             "mailinglist.php",
+	             { "email": email, "site": site, "narrative": "first"},
+	             function(data) {
+	                localStorage.setItem('newsletter', site);
+	                $('#modal_content').html(data);
+	             }
+	
+	        );
+	        return false;
+		});
 	}
-	
-
-	
 });
