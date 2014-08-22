a93aa6cd4b6d700a8d99e31de4f8662ea16e886c
josephrmoore <josephrmoore@gmail.com>
2014-08-22 03:04:40 -0400

    badge ajax fix

--- download/badge.php
+++ download/badge.php
@@ -1,11 +1,10 @@
 <?php
-	// requires php5
-	define('UPLOAD_DIR', 'users/');
-	$img = $_POST['img'];
-	$parts = explode(',', $img);  
-    $data = $parts[1];
-	$data = base64_decode($img);
-	$file = UPLOAD_DIR . uniqid() . '.png';
-	$success = file_put_contents($file, $data);
-	print $success ? $file : 'Unable to save the file.';
+$upload_dir = "users/";
+$img = $_POST['hidden_data'];
+$img = str_replace('data:image/png;base64,', '', $img);
+$img = str_replace(' ', '+', $img);
+$data = base64_decode($img);
+$file = $upload_dir . mktime() . ".png";
+$success = file_put_contents($file, $data);
+print $success ? $file : 'Unable to save the file.';
 ?>
